9d8b3ca1ba0f57e8afba189e06f70b7475f066fe
Ross Freeman <rfreeman1@me.com>
2017-01-19 10:16:22 -0500

    Initial commit

---
+++ Phoenix/ListSelectionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Phoenix
+//
+//  Created by Ross Freeman on 1/19/17.
+//  Copyright © 2017 Ross Freeman. All rights reserved.
+//
+
+import UIKit
+
+class ListSelectionViewController: UIViewController {
+    
+    @IBOutlet weak var listCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
