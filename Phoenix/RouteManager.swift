a6b6184281a3100ebba2c921bc3643d180972c0f
Ross Freeman <rfreeman1@me.com>
2017-02-25 22:08:02 -0500

    Basic setup

---
+++ Phoenix/RouteManager.swift
@@ -0,0 +1,19 @@
+//
+//  RouteManager.swift
+//  Phoenix
+//
+//  Created by Ross Freeman on 2/25/17.
+//  Copyright © 2017 Ross Freeman. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class RouteManager {
+    let agencyId = 72
+    let baseURL = "https://transloc-api-1-2.p.mashape.com/"
+    
+    func getRoutes() {
+        
+    }
+}
