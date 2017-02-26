1c1c99e16b9c054c115d28a9cb79295b2c1de0e6
Ross Freeman <rfreeman1@me.com>
2017-02-25 22:20:30 -0500

    Cleanup request

--- Phoenix/RouteManager.swift
+++ Phoenix/RouteManager.swift
@@ -12,8 +12,19 @@ import Alamofire
 class RouteManager {
     let agencyId = 72
     let baseURL = "https://transloc-api-1-2.p.mashape.com/"
+    let headers = ["X-Mashape-Key": Keys.transLocKey, "Accept" : "application/json"]
     
     func getRoutes() {
+        let endPoint = "\(baseURL)routes.json"
+        let parameters = ["agencies": agencyId]
+        
+        Alamofire.request(endPoint, parameters: parameters, headers: headers).responseJSON { (response) in
+            debugPrint(response)
+        }
+        
+    }
+    
+    func getVehicles(routes: [String]) {
         
     }
 }
